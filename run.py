3e09437ab33690e8c6a644b3c79668ed46df123d
seanf316 <seanfinn316@gmail.com>
2022-11-21 02:06:25 +0000

    Fixed the main function in run.py

--- run.py
+++ run.py
@@ -2,9 +2,7 @@
 Run Game
 """
 
-from game import welcome_screen
-import functions
+import adventure
 
 if __name__ == '__main__':
-    functions.clear_display()
-    welcome_screen()
+    adventure.main()
